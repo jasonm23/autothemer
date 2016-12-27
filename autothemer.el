13d1eafc041c9525c1fe558e11ba8ab0eb313bbe
Jason Milkins <jasonm23@users.noreply.github.com>
2016-12-27 21:48:27 +0700

    Bump package version to 0.2.2

--- autothemer.el
+++ autothemer.el
@@ -4,7 +4,7 @@
 
 ;; Author: Sebastian Sturm
 ;; URL: https://github.com/sebastiansturm/autothemer
-;; Version: 0.2
+;; Version: 0.2.2
 ;; Package-Requires: ((dash "2.10.0") (emacs "24") (cl-lib "0.5"))
 
 ;;; Commentary:
