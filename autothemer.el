56ed98f6907076b2061712951c95da62c21d08f9
Sebastian Sturm <mail@sebastian-sturm.de>
2017-01-22 15:52:38 +0100

    add support for XYZ and Lab color spaces (untested)

--- autothemer.el
+++ autothemer.el
@@ -17,6 +17,13 @@
 (require 'cl-lib)
 (require 'dash)
 
+(defcustom autothemer-color-metric 'Lab
+  "Color space within which autothemer determines color distances
+ (using the Euclidean distance metric). Possible values are 'rgb, 'xyz and 'Lab."
+  :type '(choice (const :tag "Lab" Lab)
+                 (const :tag "RGB" rgb)
+                 (const :tag "XYZ" xyz))
+  :group 'autothemer)
 (cl-defstruct autothemer--color name value)
 
 (cl-defstruct autothemer--theme colors defined-faces)
@@ -103,28 +110,71 @@ bindings within both the REDUCED-SPECS and the BODY."
                                            (elt ,face-specs ,temp-n))))))
     face-customizer))
 
-(defun autothemer--color-distance (color autothemer-color)
-  "Return the distance in rgb space between COLOR and AUTOTHEMER-COLOR.
-Here, COLOR is an Emacs color specification and AUTOTHEMER-COLOR is of
-type `autothemer--color'."
-  (let ((rgb-1 (color-values color))
-        (rgb-2 (color-values (autothemer--color-value autothemer-color))))
-    (-sum (--zip-with (abs (- it other)) rgb-1 rgb-2))))
+;; the following color conversion functions have been taken from www.brucelindbloom.com,
+;; hopefully without introducing too many transcription (or interpretation) errors
+;; along the way
+(defun autothemer--srgb-to-xyz (srgb-color)
+  "Convert an sRGB triplet to XYZ. Cf. www.brucelindbloom.com"
+  (cl-flet ((V-to-v (V) (if (<= V 0.04045) (/ V 12.92)
+                          (expt (/ (+ V 0.055) 1.055) 2.4))))
+    (let ((r (V-to-v (/ (nth 0 srgb-color) 65535.0)))
+          (g (V-to-v (/ (nth 1 srgb-color) 65535.0)))
+          (b (V-to-v (/ (nth 2 srgb-color) 65535.0))))
+      (list (+ (* 0.4124564 r) (* 0.3575761 g) (* 0.1804375 b))
+            (+ (* 0.2126729 r) (* 0.7151522 g) (* 0.0721750 b))
+            (+ (* 0.0193339 r) (* 0.1191920 g) (* 0.9503041 b))))))
+
+(defun autothemer--xyz-to-lab (xyz-color)
+  "Convert an XYZ color triplet to Lab space, cf. www.brucelindbloom.com."
+  ;; using (0.95047 1 1.08883) as the reference white point
+  (let ((eps 0.008856)
+        (kappa 903.3)
+        (x (/ (nth 0 xyz-color) 0.95047))
+        (y (nth 1 xyz-color))
+        (z (/ (nth 2 xyz-color) 1.08883)))
+    (cl-flet ((f (val) (if (> val eps) (expt val 0.333333)
+                         (/ (+ 16 (* kappa val)) 116))))
+      (list (- (* 116 (f y)) 16)
+            (* 500 (- (f x) (f y)))
+            (* 200 (- (f y) (f z)))))))
+
+(defun autothemer--srgb-to-lab (srgb-color)
+  "Convert an sRGB color triplet to Lab space, cf. www.brucelindbloom.com."
+  (autothemer--xyz-to-lab (autothemer--srgb-to-xyz srgb-color)))
+
+(defun autothemer--color-distance (color autothemer-color color-fn)
+  "Return the euclidean distance between COLOR and AUTOTHEMER-COLOR,
+where COLOR is an Emacs color specification, AUTOTHEMER-COLOR is of
+type `autothemer--color' and COLOR-FN is a function mapping rgb values
+(with all components ranging between 0 and 65535) to the three-dimensional
+target color space. If COLOR-FN is the identity function,
+`autothemer--color-distance' effectively measures the Euclidean distance
+in RGB space. Using the conversion functions `autothemer--srgb-to-xyz' and
+`autothemer--srgb-to-lab', the conversion can alternatively be performed in
+the XYZ or Lab color space."
+  (let* ((rgb-1 (color-values color))
+         (rgb-2 (color-values (autothemer--color-value autothemer-color)))
+         (coords-1 (funcall color-fn rgb-1))
+         (coords-2 (funcall color-fn rgb-2)))
+    (-sum (--zip-with (abs (- it other)) coords-1 coords-2))))
 
 (defun autothemer--find-closest-color (colors color)
   "Return the element of COLORS that is closest in rgb space to COLOR.
 Here, COLOR is an Emacs color specification and COLORS is a list
 of `autothemer--color' structs."
-  (let ((mindistance 0)
-        (closest-color nil))
-    (mapc (lambda (candidate)
-            (when (color-defined-p (autothemer--color-value candidate))
-              (let ((distance (autothemer--color-distance color candidate)))
-                (if (or (not closest-color) (< distance mindistance))
-                    (setq closest-color candidate
-                          mindistance distance)))))
-          colors)
-    closest-color))
+  (cl-flet ((color-fn (case autothemer-color-metric
+                        ('rgb #'identity)
+                        ('xyz #'autothemer--srgb-to-xyz)
+                        ('Lab #'autothemer--srgb-to-lab))))
+    (let ((mindistance 0) (closest-color nil))
+      (mapc (lambda (candidate)
+              (when (color-defined-p (autothemer--color-value candidate))
+                (let ((distance (autothemer--color-distance color candidate #'color-fn)))
+                  (if (or (not closest-color) (< distance mindistance))
+                      (setq closest-color candidate
+                            mindistance distance)))))
+            colors)
+      closest-color)))
 
 (defun autothemer--unthemed-faces ()
   "Find uncustomized faces.
@@ -216,15 +266,20 @@ approximate the faces' current definitions using the color
 palette used in the most recent invocation of
 `autothemer-deftheme'."
   (interactive)
-  (let* ((missing-faces (autothemer--unthemed-faces))
-         (templates (--map (autothemer--approximate-spec
-                            (autothemer--alist-to-reduced-spec
-                             it (autothemer--face-to-alist it))
-                            autothemer--current-theme)
-                           missing-faces))
-         (buffer (get-buffer-create (generate-new-buffer-name "*Autothemer: unthemed faces*"))))
-    (with-current-buffer buffer (emacs-lisp-mode) (insert (pp templates)))
-    (switch-to-buffer buffer)))
+  (if (and (boundp 'autothemer--current-theme)
+           autothemer--current-theme)
+      (let* ((missing-faces (autothemer--unthemed-faces))
+             (templates (--map (autothemer--approximate-spec
+                                (autothemer--alist-to-reduced-spec
+                                 it (autothemer--face-to-alist it))
+                                autothemer--current-theme)
+                               missing-faces))
+             (buffer (get-buffer-create (generate-new-buffer-name
+                                         "*Autothemer: unthemed faces*"))))
+        (with-current-buffer buffer (emacs-lisp-mode) (insert (pp templates)))
+        (switch-to-buffer buffer))
+    (message "Error: for `autothemer-generate-templates' to work, you first need to define
+a theme using `autothemer-deftheme'.")))
 
 (defun autothemer--append-column (list-of-lists new-column)
   "If LIST-OF-LISTS is nil, return NEW-COLUMN.  Otherwise, append to every element of LIST-OF-LISTS the corresponding element of NEW-COLUMN."
